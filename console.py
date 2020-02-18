bbdb466287881404a0802d40182c7816f80f1f32
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-02-18 15:35:32 +0000

    changes so far

--- console.py
+++ console.py
@@ -10,32 +10,51 @@ class HBNBCommand(cmd.Cmd):
     """ the console """
     prompt = '(hbnb)'
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
+    def do_quit(self):
+        """
+        Quit command to exit the program
+        """
         return True
 
-    def do_EOF(self, arg):
-        """ ummm """
+    def do_EOF(self):
+        """
+        Exit command
+        """
         return True
 
-    def do_create(self, arg):
-        """ creates a new instance of basemodel"""
+    def do_create(classname):
+        """
+        creates a new instance of basemodel
+        arg: Class name
+        """
         pass
 
-    def do_show(self, arg):
-        """ prints the string representation of an instance """
+    def do_show(classname, classid):
+        """
+        prints the string representation of an instance
+        arg 1: class name | arg 2: di
+        """
         pass
 
-    def do_destroy(self, arg):
-        """ Deletes an instance based on the class name"""
+    def do_destroy(classname, classid):
+        """
+        Deletes an instance based on the class name
+        arg 1: class name | arg 2: id
+        """
         pass
 
-    def do_all(self, arg):
-        """ Prints all string representation of all instances"""
+    def do_all(classname):
+        """
+        Prints all string representation of all instances
+        argument (optional): class name
+        """
         pass
 
     def do_update(self, arg):
-        """ updates an instance based on id"""
+        """
+        updates an instance based on name and  id
+        arg 1: class name | arg 2: id
+        """
         pass
 
 
